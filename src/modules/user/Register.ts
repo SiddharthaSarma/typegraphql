636fe10932bd43059df51e29c2780452875861b3
siddharthasarma <sidduinevitable@gmail.com>
2019-01-13 10:11:45 +0530

    Moved register mutation inputs into separate file

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,6 +1,7 @@
 import { Resolver, Query, Mutation, Arg } from 'type-graphql';
 import { User } from '../../entity/User';
 import bcrypt from 'bcryptjs';
+import { RegisterInput } from './register/RegisterInput';
 
 @Resolver()
 export class HelloResolver {
@@ -11,17 +12,18 @@ export class HelloResolver {
   }
 
   @Mutation(() => User)
-  async registerUser(
-    @Arg('firstName') firstName: string,
-    @Arg('lastName') lastName: string,
-    @Arg('email') email: string,
-    @Arg('password') password: string
-  ): Promise<User> {
+  async registerUser(@Arg('input')
+  {
+    firstName,
+    lastName,
+    email,
+    password
+  }: RegisterInput): Promise<User> {
     const hasedPassword = await bcrypt.hash(password, 12);
     const user = User.create({
-      firstName,
-      lastName,
-      email,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
       password: hasedPassword
     }).save();
     return user;
