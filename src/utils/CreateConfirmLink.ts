08971d735354acf1621b54e05280d37211cbe0c4
siddharthasarma <sidduinevitable@gmail.com>
2019-02-04 06:16:56 +0530

    Added prefixes to token

--- src/utils/CreateConfirmLink.ts
+++ src/utils/CreateConfirmLink.ts
@@ -1,8 +1,9 @@
+import { confirmationPrefix } from 'src/modules/constants/redisPrefixer';
 import { v4 } from 'uuid';
 import { redis } from '../redis';
 
 export const createConfirmLink = async (userId: number): Promise<string> => {
   const token = v4();
-  await redis.set(token, userId, 'ex', 60 * 60 * 24);
+  await redis.set(confirmationPrefix + token, userId, 'ex', 60 * 60 * 24);
   return `http://localhost:3000/user/confirm/${token}`;
 };
